cd7bba90071503a4f0af9e3b069a5093eaa39a51
Pierre-Yves Fare <pyfare@gmail.com>
2019-11-07 18:13:02 +0000

    Implement IAsyncLifeTime to support async test fixtures.

--- arangodb-net-standard.Test/CollectionApi/CollectionApiClientTestFixture.cs
+++ arangodb-net-standard.Test/CollectionApi/CollectionApiClientTestFixture.cs
@@ -1,15 +1,24 @@
-﻿using ArangoDBNetStandard;
+﻿using System.Threading.Tasks;
+using ArangoDBNetStandard;
 
 namespace ArangoDBNetStandardTest.CollectionApi
 {
-    public class CollectionApiClientTestFixture: ApiClientTestFixtureBase
+    public class CollectionApiClientTestFixture : ApiClientTestFixtureBase
     {
         public ArangoDBClient ArangoDBClient { get; internal set; }
 
         public CollectionApiClientTestFixture()
         {
+        }
+
+        public override async Task InitializeAsync()
+        {
+            await base.InitializeAsync();
+
             string dbName = nameof(CollectionApiClientTestFixture);
-            CreateDatabase(dbName);
+
+            await CreateDatabase(dbName);
+
             ArangoDBClient = GetArangoDBClient(dbName);
         }
     }
