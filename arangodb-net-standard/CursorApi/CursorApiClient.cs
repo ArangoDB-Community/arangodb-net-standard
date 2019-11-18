6a6ae171ab670fd746efe9ba7268c31e0715b896
Alistair Bowen-Davies <abowendavies@actify.com>
2019-11-18 16:28:42 +0000

    Shorten line length in PutCursorAsync Task/#97

--- arangodb-net-standard/CursorApi/CursorApiClient.cs
+++ arangodb-net-standard/CursorApi/CursorApiClient.cs
@@ -98,7 +98,8 @@ namespace ArangoDBNetStandard.CursorApi
 
         public async Task<PutCursorResponse<T>> PutCursorAsync<T>(string cursorId)
         {
-            using (var response = await _client.PutAsync(_cursorApiPath + "/" + WebUtility.UrlEncode(cursorId), new StringContent(string.Empty)))
+            var uriString = _cursorApiPath + "/" + WebUtility.UrlEncode(cursorId);
+            using (var response = await _client.PutAsync(uriString, new StringContent(string.Empty)))
             {
                 if (response.IsSuccessStatusCode)
                 {
