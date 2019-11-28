901ab0d4f4ef7c881b36606605b06b2018b76a2d
Pierre-Yves Fare <pyfare@gmail.com>
2019-11-28 18:02:37 +0000

    Add comments on new serialization interface and class.

--- arangodb-net-standard/Serialization/IContentSerialization.cs
+++ arangodb-net-standard/Serialization/IContentSerialization.cs
@@ -2,10 +2,29 @@
 
 namespace ArangoDBNetStandard.Serialization
 {
+    /// <summary>
+    /// Defines a serialization layer used for the content in transport.
+    /// </summary>
     public interface IContentSerialization
     {
+        /// <summary>
+        /// Deserializes the JSON structure contained by the specified stream
+        /// into an instance of the specified type.
+        /// </summary>
+        /// <typeparam name="T">The type of the object to deserialize to.</typeparam>
+        /// <param name="stream">The stream containing the JSON structure to deserialize.</param>
+        /// <returns></returns>
         T DeserializeJsonFromStream<T>(Stream stream);
 
+        /// <summary>
+        /// Serializes the specified object to a JSON string,
+        /// following the provided rules for camel case property name and null value handling.
+        /// </summary>
+        /// <typeparam name="T">The type of the object to serialize.</typeparam>
+        /// <param name="item">The object to serialize.</param>
+        /// <param name="useCamelCasePropertyNames">Whether property names should be camel cased.</param>
+        /// <param name="ignoreNullValues">Whether null values should be ignored.</param>
+        /// <returns></returns>
         string SerializeToJson<T>(T item, bool useCamelCasePropertyNames, bool ignoreNullValues);
     }
 }
--- arangodb-net-standard/Serialization/JsonNetContentSerialization.cs
+++ arangodb-net-standard/Serialization/JsonNetContentSerialization.cs
@@ -4,8 +4,18 @@ using System.IO;
 
 namespace ArangoDBNetStandard.Serialization
 {
+    /// <summary>
+    /// Implements a <see cref="IContentSerialization"/> that uses Json.NET.
+    /// </summary>
     public class JsonNetContentSerialization : IContentSerialization
     {
+        /// <summary>
+        /// Deserializes the JSON structure contained by the specified stream
+        /// into an instance of the specified type.
+        /// </summary>
+        /// <typeparam name="T">The type of the object to deserialize to.</typeparam>
+        /// <param name="stream">The stream containing the JSON structure to deserialize.</param>
+        /// <returns></returns>
         public virtual T DeserializeJsonFromStream<T>(Stream stream)
         {
             if (stream == null || stream.CanRead == false)
@@ -24,6 +34,15 @@ namespace ArangoDBNetStandard.Serialization
             }
         }
 
+        /// <summary>
+        /// Serializes the specified object to a JSON string,
+        /// following the provided rules for camel case property name and null value handling.
+        /// </summary>
+        /// <typeparam name="T">The type of the object to serialize.</typeparam>
+        /// <param name="item">The object to serialize.</param>
+        /// <param name="useCamelCasePropertyNames">Whether property names should be camel cased.</param>
+        /// <param name="ignoreNullValues">Whether null values should be ignored.</param>
+        /// <returns></returns>
         public virtual string SerializeToJson<T>(
             T item,
             bool useCamelCasePropertyNames,
