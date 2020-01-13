04ee71bdd720e3aedd28fceaddc11418b19e06ba
Ross Mills <rmills@actify.com>
2020-01-13 13:54:53 +0000

    Introduce special handling for dictionary serialization
    bug/#189
    
    This prevents camel-casing or ignoring null values for user-specified
    values used for cursor request bindVars or transaction params.

---
+++ arangodb-net-standard/Serialization/DictionaryValueConverter.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace ArangoDBNetStandard.Serialization
+{
+    /// <summary>
+    /// Provides special handling for dictionaries where we do not want to camel-case convert 
+    /// nor ignore null values upon serialization.
+    /// </summary>
+    public class DictionaryValueConverter : JsonConverter
+    {
+        private static JsonSerializer _serializer = new JsonSerializer
+        {
+            NullValueHandling = NullValueHandling.Include
+        };
+
+        public override bool CanConvert(Type objectType)
+        {
+            return true;
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            return serializer.Deserialize(reader);
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            // Use our local serializer for writing instead of the passed-in serializer
+            _serializer.Serialize(writer, value);
+        }
+    }
+
+}
