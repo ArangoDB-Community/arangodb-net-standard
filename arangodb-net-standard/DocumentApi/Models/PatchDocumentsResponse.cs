965ff7e693272dcfa85a846812fadf3da3c3d162
Pierre-Yves FARE <pyfare@gmail.com>
2020-07-11 21:46:09 +0100

    Add _oldRev property in PatchDocumentsResponse.
    fix #269

--- arangodb-net-standard/DocumentApi/Models/PatchDocumentsResponse.cs
+++ arangodb-net-standard/DocumentApi/Models/PatchDocumentsResponse.cs
@@ -1,11 +1,7 @@
 ﻿namespace ArangoDBNetStandard.DocumentApi.Models
 {
-    public class PatchDocumentsResponse<U> : DocumentBase
+    public class PatchDocumentsResponse<T> : PatchDocumentResponse<T>
     {
-        public U New { get; set; }
-
-        public U Old { get; set; }
-
         public bool Error { get; set; }
 
         /// <summary>
