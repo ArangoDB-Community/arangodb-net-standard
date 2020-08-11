62d249f4228430df6000f083a82198585557bbed
holoncom <github.rsiera@holoncom.eu>
2020-08-11 14:51:22 +0200

    Create and throw dedicated serialization exception. fix #274

---
+++ arangodb-net-standard/Serialization/SerializationException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace ArangoDBNetStandard.Serialization
+{
+    [Serializable]
+    public class SerializationException : Exception
+    {
+        public SerializationException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
