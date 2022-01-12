3496c7444be491985c80bd71128a1db53ff2ff68
Pierre-Yves FARE <pyfare@gmail.com>
2022-01-12 13:13:08 +0000

    Make header properties optional in cursor API client interface. (#344)
    fix #343

--- arangodb-net-standard/CursorApi/ICursorApiClient.cs
+++ arangodb-net-standard/CursorApi/ICursorApiClient.cs
@@ -41,7 +41,7 @@ namespace ArangoDBNetStandard.CursorApi
         /// <param name="headerProperties">Optional. Additional Header properties.</param>
         /// <returns></returns>
         Task<CursorResponse<T>> PostCursorAsync<T>(
-            PostCursorBody postCursorBody, CursorHeaderProperties headerProperties);
+            PostCursorBody postCursorBody, CursorHeaderProperties headerProperties = null);
 
         /// <summary>
         /// Deletes an existing cursor and frees the resources associated with it.
