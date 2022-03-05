ef93f71f100bb333260e49dd2c74a4f149744725
tjoubert <terence.joubert@outlook.com>
2022-03-05 14:23:47 +0400

    Fixed GetSlowAqlQueriesResponse

--- arangodb-net-standard/AqlFunctionApi/Models/GetSlowAqlQueriesResponse.cs
+++ arangodb-net-standard/AqlFunctionApi/Models/GetSlowAqlQueriesResponse.cs
@@ -2,21 +2,38 @@
 
 namespace ArangoDBNetStandard.AqlFunctionApi.Models
 {
+    ///// <summary>
+    ///// Response from
+    ///// <see cref="AqlFunctionApiClient.GetSlowAqlQueriesAsync(GetSlowAqlQueriesQuery)"/>
+    ///// </summary>
+    //public class GetSlowAqlQueriesResponse:ResponseBase
+    //{
+    //    /// <summary>
+    //    /// An array containing the last AQL queries that are finished and 
+    //    /// have exceeded the slow query threshold in the selected database. 
+    //    /// The maximum amount of queries in the list can be controlled by 
+    //    /// setting the query tracking property maxSlowQueries. 
+    //    /// The threshold for treating a query as slow can be adjusted by 
+    //    /// setting the query tracking property slowQueryThreshold.
+    //    /// </summary>
+    //    public IList<GetSlowAqlQueriesResponseResult> Results { get; set; }
+    //}
+
+
     /// <summary>
     /// Response from
     /// <see cref="AqlFunctionApiClient.GetSlowAqlQueriesAsync(GetSlowAqlQueriesQuery)"/>
     /// </summary>
-    public class GetSlowAqlQueriesResponse:ResponseBase
+    public class GetSlowAqlQueriesResponse : List<GetSlowAqlQueriesResponseResult>
     {
-        /// <summary>
-        /// An array containing the last AQL queries that are finished and 
-        /// have exceeded the slow query threshold in the selected database. 
-        /// The maximum amount of queries in the list can be controlled by 
-        /// setting the query tracking property maxSlowQueries. 
-        /// The threshold for treating a query as slow can be adjusted by 
-        /// setting the query tracking property slowQueryThreshold.
-        /// </summary>
-        public IList<GetSlowAqlQueriesResponseResult> Results { get; set; }
+        ///// <summary>
+        ///// An array containing the last AQL queries that are finished and 
+        ///// have exceeded the slow query threshold in the selected database. 
+        ///// The maximum amount of queries in the list can be controlled by 
+        ///// setting the query tracking property maxSlowQueries. 
+        ///// The threshold for treating a query as slow can be adjusted by 
+        ///// setting the query tracking property slowQueryThreshold.
+        ///// </summary>
+        //public IList<GetSlowAqlQueriesResponseResult> Results { get; set; }
     }
-
 }
