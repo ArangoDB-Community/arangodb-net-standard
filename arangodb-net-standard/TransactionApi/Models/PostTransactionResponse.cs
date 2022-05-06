c56d5d85daac2b4b2de40b9cf4819a2f33bb8eb4
Pierre-Yves FARE <pyfare@gmail.com>
2022-05-06 19:27:52 +0100

    Resolve build warnings caused by syntax issues in XML doc comments.
    fix #372

--- arangodb-net-standard/TransactionApi/Models/PostTransactionResponse.cs
+++ arangodb-net-standard/TransactionApi/Models/PostTransactionResponse.cs
@@ -13,7 +13,7 @@ namespace ArangoDBNetStandard.TransactionApi.Models
         /// </summary>
         /// <remarks>
         /// Note that in cases where an error occurs, the ArangoDBNetStandard
-        /// client will throw an <see cref="ApiErrorException"> rather than
+        /// client will throw an <see cref="ApiErrorException"/> rather than
         /// populating this property. A try/catch block should be used instead
         /// for any required error handling.
         /// </remarks>
