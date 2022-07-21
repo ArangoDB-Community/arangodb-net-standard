49cb263d4096d84a980cbbe8ff045f15bccb6289
tjoubert <terence.joubert@outlook.com>
2022-07-21 20:01:12 +0400

    Updated remarks on all Error properties

--- arangodb-net-standard/CursorApi/Models/PutCursorResponse.cs
+++ arangodb-net-standard/CursorApi/Models/PutCursorResponse.cs
@@ -38,8 +38,14 @@ namespace ArangoDBNetStandard.CursorApi.Models
         public long Count { get; set; }
 
         /// <summary>
-        /// Indicates whether an error occurred (false in this case).
+        /// Indicates whether an error occurred
         /// </summary>
+        /// <remarks>
+        /// Note that in cases where an error occurs, the ArangoDBNetStandard
+        /// client will throw an <see cref="ApiErrorException"/> rather than
+        /// populating this property. A try/catch block should be used instead
+        /// for any required error handling.
+        /// </remarks>
         public bool Error { get; set; }
 
         /// <summary>
--- arangodb-net-standard/GraphApi/Models/GetVertexResponse.cs
+++ arangodb-net-standard/GraphApi/Models/GetVertexResponse.cs
@@ -8,6 +8,15 @@ namespace ArangoDBNetStandard.GraphApi.Models
 
         public T Vertex { get; set; }
 
+        /// <summary>
+        /// Indicates whether an error occurred
+        /// </summary>
+        /// <remarks>
+        /// Note that in cases where an error occurs, the ArangoDBNetStandard
+        /// client will throw an <see cref="ApiErrorException"/> rather than
+        /// populating this property. A try/catch block should be used instead
+        /// for any required error handling.
+        /// </remarks>
         public bool Error { get; set; }
     }
 }
