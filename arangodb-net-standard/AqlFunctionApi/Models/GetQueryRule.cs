6dc70b50f814efd309ec2d3fd6763673d80d629c
tjoubert <terence.joubert@outlook.com>
2022-10-05 20:54:24 +0400

    Updated comments to GetQueryRuleFlags and GetQueryRule

--- arangodb-net-standard/AqlFunctionApi/Models/GetQueryRule.cs
+++ arangodb-net-standard/AqlFunctionApi/Models/GetQueryRule.cs
@@ -3,12 +3,20 @@
 namespace ArangoDBNetStandard.AqlFunctionApi.Models
 {
     /// <summary>
-    /// Represents a response item returned by 
+    /// Describes an AQL optimizer rule. 
+    /// This is a response item returned by 
     /// <see cref="IAqlFunctionApiClient.GetQueryRulesAsync(System.Threading.CancellationToken)"/>
     /// </summary>
     public class GetQueryRule
     {
+        /// <summary>
+        /// The name of the optimizer rule as seen in query explain outputs.
+        /// </summary>
         public string Name { get; set; }
+
+        /// <summary>
+        /// An object with the properties of the rule.
+        /// </summary>
         public GetQueryRuleFlags Flags { get; set; }
     }
 }
