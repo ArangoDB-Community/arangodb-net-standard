3a23d084e62a14b15decfb6b5894e8ef4bd65e8f
Jonas Ledel <jonas@fabled.se>
2022-10-26 19:13:26 +0200

    Fixes #436 Properties in RunningAqlQuery class should be PascalCase instead of lowerCase

--- arangodb-net-standard/AqlFunctionApi/Models/RunningAqlQuery.cs
+++ arangodb-net-standard/AqlFunctionApi/Models/RunningAqlQuery.cs
@@ -17,39 +17,39 @@ namespace ArangoDBNetStandard.AqlFunctionApi.Models
         /// <summary>
         /// The name of the database the query runs in.
         /// </summary>
-        public string database { get; set; }
+        public string Database { get; set; }
 
         /// <summary>
         /// The name of the user that started the query.
         /// </summary>
-        public string user { get; set; }
+        public string User { get; set; }
 
         /// <summary>
         /// The query string (potentially truncated).
         /// </summary>
-        public string query { get; set; }
+        public string Query { get; set; }
 
         /// <summary>
         /// The bind parameter values used by the query.
         /// </summary>
-        public Dictionary<string, object> bindVars { get; set; }
+        public Dictionary<string, object> BindVars { get; set; }
 
         /// <summary>
         /// The date and time when the query was started. 
         /// </summary>
-        public DateTime? started { get; set; }
+        public DateTime? Started { get; set; }
 
         /// <summary>
         /// The query’s run time up to the point the list of queries was queried.
         /// </summary>
-        public string runTime { get; set; }
+        public string RunTime { get; set; }
 
         /// <summary>
         /// The query’s current execution state.
         /// See online documentation 
         /// for more information.
         /// </summary>
-        public string state { get; set; }
+        public string State { get; set; }
 
         /// <summary>
         /// Indicates whether or not the query uses a streaming cursor.
