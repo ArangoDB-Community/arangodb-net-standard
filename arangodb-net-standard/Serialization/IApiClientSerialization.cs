2dc8d2dbaebbfc434e7cb2e0cc8b8e77b7aa7f1a
tjoubert <terence.joubert@outlook.com>
2022-12-05 14:50:08 +0400

    Removed unused methods from IApiClientSerialization

--- arangodb-net-standard/Serialization/IApiClientSerialization.cs
+++ arangodb-net-standard/Serialization/IApiClientSerialization.cs
@@ -9,15 +9,6 @@ namespace ArangoDBNetStandard.Serialization
     public interface IApiClientSerialization
     {
         /// <summary>
-        /// Deserializes the data structure contained by the specified stream
-        /// into an instance of the specified type.
-        /// </summary>
-        /// <typeparam name="T">The type of the object to deserialize to.</typeparam>
-        /// <param name="stream">The stream containing the JSON structure to deserialize.</param>
-        /// <returns></returns>
-        //T DeserializeFromStream<T>(Stream stream);
-
-        /// <summary>
         /// Asynchronously deserializes the data structure contained by the specified stream
         /// into an instance of the specified type.
         /// </summary>
@@ -25,17 +16,6 @@ namespace ArangoDBNetStandard.Serialization
         /// <param name="stream">The stream containing the JSON structure to deserialize.</param>
         /// <returns></returns>
         Task<T> DeserializeFromStreamAsync<T>(Stream stream);
-
-        /// <summary>
-        /// Serializes the specified object to a sequence of bytes,
-        /// following the provided rules for camel case property name and null value handling.
-        /// </summary>
-        /// <typeparam name="T">The type of the object to serialize.</typeparam>
-        /// <param name="item">The object to serialize.</param>
-        /// <param name="serializationOptions">The serialization options. When the value is null the
-        /// the serialization options should be provided by the serializer, otherwise the given options should be used.</param>
-        /// <returns></returns>
-        //byte[] Serialize<T>(T item, ApiClientSerializationOptions serializationOptions);
 
         /// <summary>
         /// Asynchronously serializes the specified object to a sequence of bytes,
@@ -47,17 +27,6 @@ namespace ArangoDBNetStandard.Serialization
         /// the serialization options should be provided by the serializer, otherwise the given options should be used.</param>
         /// <returns></returns>
         Task<byte[]> SerializeAsync<T>(T item, ApiClientSerializationOptions serializationOptions);
-
-        /// <summary>
-        /// Serializes the specified object to a JSON string,
-        /// following the provided rules for camel case property name and null value handling.
-        /// </summary>
-        /// <typeparam name="T">The type of the object to serialize.</typeparam>
-        /// <param name="item">The object to serialize.</param>
-        /// <param name="serializationOptions">The serialization options. When the value is null the
-        /// the serialization options should be provided by the serializer, otherwise the given options should be used.</param>
-        /// <returns></returns>
-        //string SerializeToString<T>(T item, ApiClientSerializationOptions serializationOptions);
 
         /// <summary>
         /// Asynchronously serializes the specified object to a JSON string,
