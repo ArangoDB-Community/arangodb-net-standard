1c8cce8253206e1b0fc8cc4f71d4349c487a3380
tjoubert <terence.joubert@outlook.com>
2022-12-21 21:28:14 +0400

    Added missing ngram properties

--- arangodb-net-standard/AnalyzerApi/Models/AnalyzerProperties.cs
+++ arangodb-net-standard/AnalyzerApi/Models/AnalyzerProperties.cs
@@ -9,6 +9,16 @@ namespace ArangoDBNetStandard.AnalyzerApi.Models
     public class AnalyzerProperties
     {
         /// <summary>
+        /// One byte is considered as one character (default)
+        /// </summary>
+        public const string StreamTypeBinary = "binary";
+
+        /// <summary>
+        /// One Unicode codepoint is treated as one character
+        /// </summary>
+        public const string StreamTypeUTF8 = "utf8";
+
+        /// <summary>
         /// Optional. When true, accented characters are preserved. 
         /// When false, accented characters are converted to 
         /// their base characters.
@@ -107,5 +117,45 @@ namespace ArangoDBNetStandard.AnalyzerApi.Models
         /// Optional for Classification analyzers.
         /// </summary>
         public decimal? Threshold { get; set; }
+
+        /// <summary>
+        /// Unsigned integer for the minimum n-gram length
+        /// </summary>
+        public int Min { get; set; }
+
+        /// <summary>
+        /// Unsigned integer for the maximum n-gram length
+        /// </summary>
+        public int Max { get; set; }
+
+        /// <summary>
+        /// When true: include the original value as well.
+        /// When false: produce the n-grams based on <see cref="Min"/> 
+        /// and <see cref="Max"/> only. 
+        /// </summary>
+        public bool PreserveOriginal { get; set; }
+
+        /// <summary>
+        /// This value will be prepended to n-grams which
+        /// include the beginning of the input. Can be used
+        /// for matching prefixes. Choose a character or 
+        /// sequence as marker which does not occur in the input.
+        /// </summary>
+        public string StartMarker { get; set; }
+
+        /// <summary>
+        /// This value will be appended to n-grams which 
+        /// include the end of the input. Can be used for 
+        /// matching suffixes. Choose a character or sequence
+        /// as marker which does not occur in the input.
+        /// </summary>
+        public string EndMarker { get; set; }
+
+        /// <summary>
+        /// Type of the input stream.
+        /// Possible values are <see cref="StreamTypeBinary"/>
+        /// and <see cref="StreamTypeUTF8"/>
+        /// </summary>
+        public string StreamType { get; set; }
     }
 }
