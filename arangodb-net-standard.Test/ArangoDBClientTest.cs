8af4bee398d738573641e943ab44c4cd8fcb5b87
Pierre-Yves FARE <pyfare@gmail.com>
2023-12-16 22:30:42 +0000

    Add a parameter to control disposal of provided instance when creating DB client.

---
+++ arangodb-net-standard.Test/ArangoDBClientTest.cs
@@ -0,0 +1,131 @@
+﻿using ArangoDBNetStandard;
+using ArangoDBNetStandard.Transport;
+using ArangoDBNetStandard.Transport.Http;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ArangoDBNetStandardTest
+{
+    public class ArangoDBClientTest : IClassFixture<ArangoDBClientTestFixture>
+    {
+        private readonly ArangoDBClientTestFixture _fixture;
+
+        private readonly Uri _hostUri;
+
+        public ArangoDBClientTest(ArangoDBClientTestFixture fixture)
+        {
+            _fixture = fixture;
+            _hostUri = new Uri($"http://{fixture.ArangoDbHost}:{fixture.ArangoDbPort}/");
+        }
+
+        [Fact]
+        public async Task Dispose_ShouldDisposeTransport_WhenTransportDisposalIsNotSuppressed()
+        {
+            var transport = HttpApiTransport.UsingBasicAuth(
+                _hostUri,
+                _fixture.DatabaseName,
+                _fixture.Username,
+                _fixture.Password);
+
+            var dbClient = new ArangoDBClient(
+                transport,
+                suppressTransportDisposal: false);
+
+            // Act
+
+            dbClient.Dispose();
+
+            // Assert
+
+            await Assert.ThrowsAsync<ObjectDisposedException>(
+                () => transport.GetAsync($"_db/{_fixture.DatabaseName}/_admin/echo"));
+        }
+
+        [Fact]
+        public async Task Dispose_ShouldNotDisposeTransport_WhenTransportDisposalIsSuppressed()
+        {
+            var transport = HttpApiTransport.UsingBasicAuth(
+                _hostUri,
+                _fixture.DatabaseName,
+                _fixture.Username,
+                _fixture.Password);
+
+            var dbClient = new ArangoDBClient(
+                transport,
+                suppressTransportDisposal: true);
+
+            // Act
+
+            dbClient.Dispose();
+
+            // Assert
+
+            IApiClientResponse response = await transport.GetAsync($"_admin/echo");
+
+            Assert.True(response.IsSuccessStatusCode);
+
+            transport.Dispose();
+        }
+
+        [Fact]
+        public async Task Dispose_ShouldDisposeHttpClient_WhenClientDisposalIsNotSuppressed()
+        {
+            var client = new HttpClient()
+            {
+                BaseAddress = _hostUri
+            };
+
+            var dbClient = new ArangoDBClient(
+                client,
+                suppressClientDisposal: false);
+
+            // Act
+
+            dbClient.Dispose();
+
+            // Assert
+
+            await Assert.ThrowsAsync<ObjectDisposedException>(
+                () => client.GetAsync($"_db/{_fixture.DatabaseName}/_admin/echo"));
+        }
+
+        [Fact]
+        public async Task Dispose_ShouldNotDisposeHttpClient_WhenClientDisposalIsSuppressed()
+        {
+            var client = new HttpClient()
+            {
+                BaseAddress = _hostUri
+            };
+
+            string credentials = Convert.ToBase64String(
+                Encoding.ASCII.GetBytes($"{_fixture.Username}:{_fixture.Password}"));
+
+            client.DefaultRequestHeaders.Authorization =
+                new AuthenticationHeaderValue("Basic", credentials);
+
+            var dbClient = new ArangoDBClient(
+                client,
+                suppressClientDisposal: true);
+
+            // Act
+
+            dbClient.Dispose();
+
+            // Assert
+
+            using (HttpResponseMessage response = await client.GetAsync(
+                $"_db/{_fixture.DatabaseName}/_admin/echo"))
+            {
+                Assert.True(
+                    response.IsSuccessStatusCode,
+                    $"Error response. Status code: {response.StatusCode}");
+            }
+
+            client.Dispose();
+        }
+    }
+}
---
+++ arangodb-net-standard.Test/ArangoDBClientTestFixture.cs
@@ -0,0 +1,31 @@
+﻿using ArangoDBNetStandard.DatabaseApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ArangoDBNetStandardTest
+{
+    public class ArangoDBClientTestFixture : ApiClientTestFixtureBase
+    {
+        public string DatabaseName { get; } = nameof(ArangoDBClientTestFixture);
+
+        public string Username { get; } = Guid.NewGuid().ToString();
+
+        public string Password { get; } = "abcxyz";
+
+        public override async Task InitializeAsync()
+        {
+            await base.InitializeAsync();
+            await CreateDatabase(
+                DatabaseName,
+                new List<DatabaseUser>
+                {
+                    new()
+                    {
+                        Username = Username,
+                        Passwd = Password
+                    }
+                });
+        }
+    }
+}
