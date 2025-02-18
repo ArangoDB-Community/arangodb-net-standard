86523fc4c802c49565dcd4b03a876bb44e8cedff
Pierre-Yves FARE <pyfare@gmail.com>
2025-02-18 09:42:57 +0000

    Update ErrorEnumGenerator to validate provided filepath.

--- ErrorEnumGenerator/Program.cs
+++ ErrorEnumGenerator/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿if (args.Length == 0)
+{
+    Console.WriteLine("Please provide a file path as an argument.");
+    Environment.Exit(1);
+}
+
+string filePath = args[0];
+
+if (!File.Exists(filePath))
+{
+    Console.WriteLine($"File not found: {filePath}");
+    Environment.Exit(1);
+}
